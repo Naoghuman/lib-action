10d55b56fa3760184167c8dbb6dc6d614cfeb46f
Naoghuman <peter.rogge@yahoo.de>
2017-05-28 19:20:25 +0200

    #33 [Impl] Create new interface `Validator`.

---
+++ src/main/java/com/github/naoghuman/lib/action/internal/Validator.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2017 Naoghuman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.lib.action.internal;
+
+import java.util.Objects;
+
+/**
+ * This interface contains different default methods to validate if an {@link java.lang.Object} 
+ * conforms specific behaviours or not. For example if an <code>Object</code> is 
+ * NULL or not.
+ *
+ * @author Naoghuman
+ * @see    java.lang.Object
+ */
+public interface Validator {
+    
+    /**
+     * Validates if the attribute <code>value</code> isn't NULL.
+     *
+     * @param value the attribute which should be validated.
+     * @param <T>   the type of the reference.
+     * @throws      NullPointerException if (value == NULL).
+     */
+    public default <T> void requireNonNull(T value) throws NullPointerException {
+        Objects.requireNonNull(value, "The attribute [value] can't be NULL"); // NOI18N
+    }
+    
+    /**
+     * Validates if the attribute <code>value</code> isn't NULL and not EMPTY.
+     *
+     * @param value the attribute which should be validated.
+     * @throws      NullPointerException if (value == NULL).
+     * @throws      IllegalArgumentException if (value.trim() == EMPTY).
+     */
+    public default void requireNonNullAndNotEmpty(String value) throws NullPointerException, IllegalArgumentException {
+        Objects.requireNonNull(value, "The attribute [value] can't be NULL"); // NOI18N
+        
+        if (value.trim().isEmpty()) {
+            throw new IllegalArgumentException("The attribute [value] can't be EMPTY"); // NOI18N
+        }
+    }
+    
+}
